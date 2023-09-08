64b39250fa0228f46f13de8b185d3348eabb85f6
Van Lam Nguyen <vanlam1412@gmail.com>
2023-09-08 21:56:11 +0700

    update

--- src/service/config.js
+++ src/service/config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-export const BASE_URL = "http://127.0.0.1:8000";
+export const BASE_URL = "https://movie-vanlam-api.onrender.com";
 
 const Token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
 
